fcfd90e4b8e75b8095518c9ce63a5ee188f92aaf
Keith Matthews <keith@alice.caltech.edu>
2017-05-26 02:21:45 -0700

    Program now works as intended.  Cypher dictionary is now hard-coded.  Now using python iterators correctly.

--- read.py
+++ read.py
@@ -1,10 +1,11 @@
 import string
 from pandas import read_csv
+from itertools import cycle
 
 # =======================================================================
 # Reads text from file in to a string, ignoring punctuation and whitespace, and converting
 # all characters to uppercase.
-def importTextAndStripToCaps(filename):
+def importTextAndStripToCaps(filename, removeSpacesQ):
     #from https://stackoverflow.com/questions/2988211/how-to-read-a-single-character-at-a-time-from-a-file-in-python
     #Also:
     #   for whitespace removal: https://www.tutorialspoint.com/python/string_isspace.htm
@@ -16,13 +17,11 @@ def importTextAndStripToCaps(filename):
         while True:
             c = handle.read(1)
             if not c:
-                #print "End of file."
                 break
-            if c.isspace():
+            if removeSpacesQ and c.isspace():
                 continue
             if c in string.punctuation:
                 continue
-            #print c.upper()
             result += c.upper()
     return result
 
@@ -33,6 +32,7 @@ def importTextAndStripToCaps(filename):
 def constructMask(msg, cde):
     result = ""
     for c in msg:
+        # @TODO - Must handle case where c is not in cde.
         result += cde[c]
     return result
 
@@ -40,43 +40,41 @@ def constructMask(msg, cde):
 # Outputs the string, txt, where the case (upper/lower) for each character is set 
 # by the value of the mask array (either '1' or '0').
 def encodeInCaps(txt, mask):
+    char = ""
     result = ""
-    txt_it = txt.iter()
+    txt_it = cycle(txt)
     for m in mask:
-        if m=='1':
-            result += txt_it.upper()
+        char = txt_it.next()
+        while char.isspace():
+            result += char
+            char = txt_it.next()
+        if m == '1':
+            result += char.upper()
         else:
-            result += txt_it.lower()
-        txt_it += 1
-        if txt_it: # @TODO - This is intended to check if we've reached the end of 
-        # the string and if so, bring us back to the beginning, but the syntax is 
-        # wrong.  Ugh.
-            txt_it = txt.begin()            
+            result += char.lower()
+
     return result
 
 # =======================================================================
 # =======================================================================
 # Main code begins here:
 
-text = importTextAndStripToCaps("lorem_ipsum.txt")
-# print text
+text = importTextAndStripToCaps("lorem_ipsum.txt", False)
+#print text
 
-message = importTextAndStripToCaps("message.txt")
+message = importTextAndStripToCaps("message.txt", True)
+#print message
 
-# we'll use pandas to read the code in from file
-# to prevent dropping leading zeros: 
-#   https://stackoverflow.com/questions/13250046/pandas-csv-import-keep-leading-zeros-in-a-column
-temp = read_csv('code.csv', sep='\t', header=1)
-#print temp
-
-code = dict(zip(temp[0], temp[1]))
+code = {'0':'00000', '1':'00001', '2':'00010', '3':'00011', '4':'00100', '5':'00101', 
+    '6':'00110', '7':'00111', '8':'01000', '9':'01001', 'A':'01010', 'B':'01011', 
+    'C':'01100', 'D':'01101', 'E':'01110', 'F':'01111', 'G':'10000', 'H':'10001',
+    'I':'10010', 'J':'10011', 'K':'10011', 'L':'10100', 'M':'10101', 'N':'10110',
+    'O':'10111', 'P':'11000', 'Q':'11000', 'R':'11001', 'S':'11010', 'T':'11011',
+    'U':'11100', 'V':'11101', 'W':'11110', 'X':'11110', 'Y':'11111', 'Z':'11111'}
 # print code
 
-# @TODO - This line should not generate an error!
-print code["C"]
-
 msgMask = constructMask(message, code)
-# print msgMask
+#print msgMask
 
 print encodeInCaps(text, msgMask)
 
